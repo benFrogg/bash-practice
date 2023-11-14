ae5e1a8bf7116c3bae9804bcafa3c70e5c5631db
Benjamin Ang <angqishao@gmail.com>
2023-11-14 21:42:35 +0800

    Updated acronym practice

--- acronym/acronym.sh
+++ acronym/acronym.sh
@@ -4,17 +4,12 @@ main () {
     args1="$1"
     length=${#args1}
     result="${args1:0:1}"
-    for ((i = 0; i < ${#args1}; i++)); do
-        increment=1
-        if [ "$i" -ne "$((length - 1))" ] && [ "${args1:i:1}" =  " " ] || [ "${args1:i:1}" = "-" ] || [ "${args1:i:1}" = "_" ]; then
-            if [[ "${args1:i+1:1}" =~ [[:alpha:]] ]]; then
-                result+="${args1:i+$increment:1}"
-            else
-                increment+=1
-            fi
+    for ((idx = 0; idx < $length; idx++)); do
+        if [[ "${args1:idx:1}" == [-_\ ] ]] && [[ "${args1:idx+1:1}" =~ [[:alpha:]] ]]; then
+            result+="${args1:idx+1:1}"
         fi
     done
-    echo "$result" | tr '[:lower:]' '[:upper:]'
+    echo ${result^^}
 }
 
 main "$@"
